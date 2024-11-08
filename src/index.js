3488adfe696098bbaf4ce6e4544e0bddd2f34382
Ramkrishna Swarnkar <128958601+ramxcodes@users.noreply.github.com>
2024-11-08 14:39:26 +0530

    Created Middleware Functions 🥸

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import { clerkMiddleware } from '@clerk/express'
 import userRoutes from "./routes/user.routes.js"
 import authRoutes from "./routes/auth.routes.js"
 import adminRoutes from "./routes/admin.routes.js"
@@ -15,6 +16,8 @@ const app = express();
 const PORT = process.env.PORT
 
 app.use(express.json());
+app.use(clerkMiddleware())
+
 
 app.use("/api/users", userRoutes)
 app.use("/api/auth", authRoutes)
