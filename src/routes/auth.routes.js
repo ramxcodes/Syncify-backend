11c4bbab1e6d198936bded61174c894f903d32be
Ramkrishna Swarnkar <128958601+ramxcodes@users.noreply.github.com>
2024-11-08 14:02:42 +0530

    Added Signup code 🌟

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,36 @@
 import { Router } from "express";
+import { User } from "../models/user.model.js";
 
 const router = Router();
 
-router.get('/', (req, res) => {
-    res.send('Auth Route with get')
-})
+router.post("/callback", async (req, res) => {
+  try {
+    const { id, firstName, lastName, imageUrl } = req.body;
+
+    const user = await User.findOne({
+      clerkId: id,
+    });
+
+    if (!user) {
+      await User.create({
+        clerkId: id,
+        fullName: `${firstName} ${lastName}`,
+        imageUrl,
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      fullName,
+      msg: "User created successfully",
+    });
+  } catch (error) {
+    console.log("Error in auth callback", error);
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error", error
+    });
+  }
+});
 
 export default router;
