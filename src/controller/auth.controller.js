356ffcd8432936ac789f76d54892d28b1fd3fe79
Ramkrishna Swarnkar <128958601+ramxcodes@users.noreply.github.com>
2024-11-09 16:36:11 +0530

    Added Error handler logic 🚫

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -1,6 +1,6 @@
 import { User } from "../models/user.model.js";
 
-export const authCallback = async (req, res) => {
+export const authCallback = async (req, res, next) => {
     try {
       const { id, firstName, lastName, imageUrl } = req.body;
   
@@ -23,9 +23,6 @@ export const authCallback = async (req, res) => {
       });
     } catch (error) {
       console.log("Error in auth callback", error);
-      res.status(500).json({
-        success: false,
-        message: "Internal Server Error", error
-      });
+      next(error);
     }
   }
