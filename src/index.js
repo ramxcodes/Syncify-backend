2f3df38a56e910f8ceed2956c8cb5e81d00c85cf
Ramkrishna Swarnkar <128958601+ramxcodes@users.noreply.github.com>
2024-11-09 16:20:01 +0530

    Added Temp file store & File upload limit 📩

--- src/index.js
+++ src/index.js
@@ -1,33 +1,46 @@
 import express from "express";
 import dotenv from "dotenv";
-import { clerkMiddleware } from '@clerk/express'
-import userRoutes from "./routes/user.routes.js"
-import authRoutes from "./routes/auth.routes.js"
-import adminRoutes from "./routes/admin.routes.js"
-import songRoutes from "./routes/songs.routes.js"
-import albumRoutes from "./routes/album.routes.js"
-import statsRoutes from "./routes/stats.routes.js"
+import fileUpload from "express-fileupload";
+import { clerkMiddleware } from "@clerk/express";
 import { connectDB } from "./lib/db.js";
+import path from "path";
 
+// Routes
+import userRoutes from "./routes/user.routes.js";
+import authRoutes from "./routes/auth.routes.js";
+import adminRoutes from "./routes/admin.routes.js";
+import songRoutes from "./routes/songs.routes.js";
+import albumRoutes from "./routes/album.routes.js";
+import statsRoutes from "./routes/stats.routes.js";
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT
+const __dirname = path.resolve();
+const PORT = process.env.PORT;
 
 app.use(express.json());
-app.use(clerkMiddleware())
+app.use(clerkMiddleware());
 
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: path.join(__dirname, "tmp"),
+    createParentPath: true,
+    limits:{
+        fileSize : 10 * 1024 * 1024 // 10 MB Limit
+    }
+  })
+);
 
-app.use("/api/users", userRoutes)
-app.use("/api/auth", authRoutes)
-app.use("/api/admin", adminRoutes)
-app.use("/api/songs", songRoutes)
-app.use("/api/albums", albumRoutes)
-app.use("/api/stats", statsRoutes)
-
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/admin", adminRoutes);
+app.use("/api/songs", songRoutes);
+app.use("/api/albums", albumRoutes);
+app.use("/api/stats", statsRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-    connectDB();
-})
+  console.log(`Server is running on port ${PORT}`);
+  connectDB();
+});
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,8 +2,9 @@ import { Router } from "express";
 
 const router = Router();
 
-router.get('/', (req, res) => {
-    res.send('User Route with get')
-})
+router.get("/", (req, res) => {
+  req.auth.userId;
+  res.send("User Route with get");
+});
 
 export default router;
