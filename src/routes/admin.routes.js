73f06a1000e8960d0fed52ba069e5f66027db170
Ramkrishna Swarnkar <128958601+ramxcodes@users.noreply.github.com>
2024-11-09 17:03:40 +0530

    Added create album route 😉

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import { protectRoutes, requireAdmin } from "../middleware/auth.middleware.js";
-import { createSong, deleteSong } from "../controller/admin.controller.js";
+import { createAlbum, createSong, deleteSong } from "../controller/admin.controller.js";
 
 const router = Router();
 
 router.post("/songs", protectRoutes, requireAdmin, createSong);
 router.delete("/songs/:id", protectRoutes, requireAdmin, deleteSong);
+
+router.post("/albums", protectRoutes, requireAdmin, createAlbum);
+
 
 export default router;
