e63eb1b40c0b49cc39f88bfa927fa306e6cee0b7
Ramkrishna Swarnkar <128958601+ramxcodes@users.noreply.github.com>
2024-11-09 16:43:05 +0530

    Added createSong middleware 🎷

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
+import { protectRoutes, requireAdmin } from "../middleware/auth.middleware.js";
+import { createSong } from "../controller/admin.controller.js";
 
 const router = Router();
 
-router.get('/', (req, res) => {
-    res.send('Admin Route with get')
-})
+router.post("/songs", protectRoutes, requireAdmin, createSong);
 
 export default router;
