bf1dc40b85a8f42657544acd4944ef996613064e
Ramkrishna Swarnkar <128958601+ramxcodes@users.noreply.github.com>
2024-11-15 00:01:04 +0530

    Created Featured, Trending, Made for you songs route

--- src/routes/songs.routes.js
+++ src/routes/songs.routes.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import { protectRoutes, requireAdmin } from "../middleware/auth.middleware.js";
-import { getAllSongs } from "../controller/song.controller.js";
+import { getAllSongs, getFeaturedSongs, getMadeForYouSongs, getTrendingSongs } from "../controller/song.controller.js";
 
 const router = Router();
 
 router.get("/", protectRoutes, requireAdmin, getAllSongs);
+router.get("/featured", getFeaturedSongs);
+router.get("/made-for-you", getMadeForYouSongs);
+router.get("/trending", getTrendingSongs);
 
 export default router;
