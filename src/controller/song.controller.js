0ae644806dd92b02ac136499692a46d798020d64
Ramkrishna Swarnkar <128958601+ramxcodes@users.noreply.github.com>
2024-11-15 00:06:08 +0530

    Added getFeaturedSongs func. 🎶

--- src/controller/song.controller.js
+++ src/controller/song.controller.js
@@ -9,3 +9,41 @@ export const getAllSongs = async (req, res, next) => {
     next(error);
   }
 };
+
+export const getFeaturedSongs = async (req, res, next) => {
+  try {
+    // fetch 6 random songs
+    const songs = await Song.aggregate([
+      {
+        $sample: { size: 6 },
+      },
+      {
+        $project: {
+          _id: 1,
+          title: 1,
+          artist: 1,
+          imageUrl: 1,
+          audioUrl: 1,
+        },
+      },
+    ]);
+
+    res.json(songs);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getMadeForYouSongs = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getTrendingSongs = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
