62b7158c97c38e4a5d02ddd64608124a3b97146f
Ramkrishna Swarnkar <128958601+ramxcodes@users.noreply.github.com>
2024-11-16 00:13:08 +0530

    Added Statics route 🗽

--- src/routes/stats.routes.js
+++ src/routes/stats.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
+import { protectRoutes, requireAdmin } from "../middleware/auth.middleware.js";
+import { getStats } from "../controller/stats.controller.js";
 
 const router = Router();
 
-router.get('/', (req, res) => {
-    res.send('Stats Route with get')
-})
+
+
+router.get("/", protectRoutes, requireAdmin,getStats);
 
 export default router;
