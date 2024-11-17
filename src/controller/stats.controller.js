68aee6da5f34857c11c77fc6d75f5871c99d8dde
Ramkrishna Swarnkar <128958601+ramxcodes@users.noreply.github.com>
2024-11-17 17:25:44 +0530

    Fixed stats Function 🧩

--- src/controller/stats.controller.js
+++ src/controller/stats.controller.js
@@ -1,47 +1,39 @@
+import { Album } from "../models/album.model.js";
 import { Song } from "../models/song.model.js";
 import { User } from "../models/user.model.js";
-import { Album } from "../models/album.model.js";
 
 export const getStats = async (req, res, next) => {
-    try {
-      // const totalSongs = await Song.countDocuments();
-      // const totalUsers = await User.countDocuments();
-      // const totalAlbums = await Album.countDocuments();
-  
-      // Optimized code
-  
-      const [totalSongs, totalAlbums, totalUsers, uniqueArtists] =
-        await Promise.all([
-          Song.countDocuments(),
-          Album.countDocuments(),
-          User.countDocuments(),
-  
-          Song.aggregate([
-            {
-              $unionWith: {
-                coll: "albums",
-                pipeline: [],
-              },
-            },
-            {
-              $group: {
-                _id: "$artist",
-              },
-            },
-            {
-              $count: "count",
-            },
-          ]),
-        ]);
-  
-  
-        res.json(200).json({
-          totalAlbums,
-          totalSongs,
-          totalUsers,
-          totalArtists: uniqueArtists[0]?.count || 0
-        })
-    } catch (error) {
-      next(error);
-    }
-  }
+	try {
+		const [totalSongs, totalAlbums, totalUsers, uniqueArtists] = await Promise.all([
+			Song.countDocuments(),
+			Album.countDocuments(),
+			User.countDocuments(),
+
+			Song.aggregate([
+				{
+					$unionWith: {
+						coll: "albums",
+						pipeline: [],
+					},
+				},
+				{
+					$group: {
+						_id: "$artist",
+					},
+				},
+				{
+					$count: "count",
+				},
+			]),
+		]);
+
+		res.status(200).json({
+			totalAlbums,
+			totalSongs,
+			totalUsers,
+			totalArtists: uniqueArtists[0]?.count || 0,
+		});
+	} catch (error) {
+		next(error);
+	}
+};
