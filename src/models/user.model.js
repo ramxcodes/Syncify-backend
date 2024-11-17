6cd1efd33ccf1f9e33a86e5a6d834415c0505a07
Ramkrishna Swarnkar <128958601+ramxcodes@users.noreply.github.com>
2024-11-17 23:26:48 +0530

    Fixed Typo in user model 🫏

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,20 +1,22 @@
 import mongoose from "mongoose";
 
-const userScheme = new mongoose.Schema({
-    fullName:{
-        type: String,
-        required: true
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
     },
-    imageUrl:{
-        type: String,
-        required: true,
+    imageUrl: {
+      type: String,
+      required: true,
     },
-    clerkId:{
-        type: String,
-        required: true,
-        unique: true
-    }
-}, {timestamps: true});
+    clerkId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  { timestamps: true }
+);
 
-
-export const User = mongoose.model('User', userScheme);
+export const User = mongoose.model("User", userSchema);
