4ae3195a152a2956217b18e5eb03a0d237b86178
Ramkrishna Swarnkar <128958601+ramxcodes@users.noreply.github.com>
2024-11-17 23:02:07 +0530

    Fixed Duplication user error 👥=👥

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -1,33 +1,24 @@
 import { User } from "../models/user.model.js";
 
 export const authCallback = async (req, res, next) => {
-  try {
-    const { id, firstName, lastName, imageUrl } = req.body;
+	try {
+		const { id, firstName, lastName, imageUrl } = req.body;
 
-    // Check if `id` is provided
-    if (!id) {
-      return res.status(400).json({ success: false, msg: "User ID is required" });
-    }
+		// check if user already exists
+		const user = await User.findOne({ clerkId: id });
 
-    const user = await User.findOne({
-      clerkId: id,
-    });
+		if (!user) {
+			// signup
+			await User.create({
+				clerkId: id,
+				fullName: `${firstName || ""} ${lastName || ""}`.trim(),
+				imageUrl,
+			});
+		}
 
-    if (!user) {
-      await User.create({
-        clerkId: id,
-        fullName: `${firstName} ${lastName}`,
-        imageUrl,
-      });
-    }
-
-    res.status(200).json({
-      success: true,
-      fullName: `${firstName} ${lastName}`,
-      msg: "User created successfully",
-    });
-  } catch (error) {
-    console.log("Error in auth callback", error);
-    next(error);
-  }
+		res.status(200).json({ success: true });
+	} catch (error) {
+		console.log("Error in auth callback", error);
+		next(error);
+	}
 };
